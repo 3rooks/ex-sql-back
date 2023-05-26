d07d430b134c54f8949e6b462b297a0a0e314c40
3rooks <Brookslynx@gmail.com>
2023-05-26 19:14:46 -0300

    🚧 docker + db connection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import { dbConnection } from '#config/db.js';
 import { httpServer } from '#config/http.js';
 
 const PORT = 8080;
 
 const bootstrap = () => {
-    httpServer.listen(PORT, () => {
+    httpServer.listen(PORT, async () => {
+        await dbConnection();
         console.log(`listening on port: ${PORT}`);
     });
 };
